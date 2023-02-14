0fc62cbed30fbfc4c377f901e466e68375c09ae8
Aamir Shaikh <aamirshaikh3232@outlook.com>
2023-02-14 21:55:22 +0530

    Add 01_Day_Introduction/ files

---
+++ 01_Day_Introduction/console.js
@@ -0,0 +1,3 @@
+console.log("Hello", "World", "!");
+console.log("HAPPY", "NEW", "YEAR", 2023);
+console.log("Welcome", "to", 30, "Days", "Of", "JavaScript");
---
+++ 01_Day_Introduction/datatypes.js
@@ -0,0 +1,5 @@
+console.log(typeof "Aamir"); // string
+console.log(typeof 5); // number
+console.log(typeof true); // boolean
+console.log(typeof null); // object type
+console.log(typeof undefined); // undefined
