97499f844ee3d3d109c0d232f83767528afa37c2
Aamir Shaikh <aamirshaikh3232@outlook.com>
2023-05-17 21:48:06 +0530

    Add 16_Day_JSON/exercises/level3/

---
+++ 16_Day_JSON/exercises/level3/level3.js
@@ -0,0 +1,119 @@
+const txt = `{
+    "Alex": {
+        "email": "alex@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 30
+    },
+    "Asab": {
+        "email": "asab@asab.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Redux",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 25,
+        "isLoggedIn": false,
+        "points": 50
+    },
+    "Brook": {
+        "email": "daniel@daniel.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux"
+        ],
+        "age": 30,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Daniel": {
+        "email": "daniel@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Python"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "John": {
+        "email": "john@john.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux",
+            "Node.js"
+        ],
+        "age": 20,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Thomas": {
+        "email": "thomas@thomas.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "Paul": {
+        "email": "paul@paul.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    }
+}
+`;
+
+// 1. Parse the txt JSON to object.
+
+const objectTxt = JSON.parse(txt);
+console.log(objectTxt);
+
+// 2. Find the user who has many skills from the variable stored in txt.
+
+const users = JSON.parse(txt);
+
+let max = 0;
+let maxSkillsUser = '';
+
+for (const user in users) {
+  const skillsCount = users[user].skills.length;
+
+  if (skillsCount > max) {
+    max = skillsCount;
+    maxSkillsUser = user;
+  }
+}
+
+console.log(`${maxSkillsUser} has the most skills with ${max} skills.`);
