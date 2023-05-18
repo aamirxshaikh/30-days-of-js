9eee5750cad84f63785723081ab4de70ddf45044
Aamir Shaikh <aamirshaikh3232@outlook.com>
2023-05-19 01:17:30 +0530

    Add 17_Day_Web_storages/exercises/level1/

---
+++ 17_Day_Web_storages/exercises/level1/level1.js
@@ -0,0 +1,7 @@
+// 1. Store you first name, last name, age, country, city in your browser localStorage.
+
+localStorage.setItem('firstName', 'Aamir');
+localStorage.setItem('lastName', 'Shaikh');
+localStorage.setItem('age', 23);
+localStorage.setItem('country', 'India');
+localStorage.setItem('city', 'Mumbai');
