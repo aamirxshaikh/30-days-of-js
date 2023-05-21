beff8618a7ee9303ee4f3afea347449a4f618213
Aamir Shaikh <aamirshaikh3232@outlook.com>
2023-05-21 15:52:51 +0530

    Add 18_Day_Promises/promise.js

---
+++ 18_Day_Promises/promise.js
@@ -0,0 +1,7 @@
+// 1. Promise
+
+// In simple terms, a Promise in JavaScript is a way to handle asynchronous operations, such as making an API call, fetching data from a server, or reading a file, which take some time to complete.
+
+// A Promise represents a future value that may not be available yet, but will eventually be resolved or rejected. It is in one of three states: pending (not yet resolved), fulfilled (successfully resolved), or rejected (failed to resolve). When a Promise is resolved or rejected, any associated functions or code that have been waiting for it will be executed.
+
+// Promises are useful because they allow you to write asynchronous code that looks and behaves like synchronous code, making it easier to manage complex code flows. By using Promises, you can avoid nested callbacks and "callback hell", and instead chain multiple operations together in a more readable and maintainable way.
