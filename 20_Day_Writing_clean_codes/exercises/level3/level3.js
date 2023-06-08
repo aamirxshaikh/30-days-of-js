ca3c4490d89a4e923b2434f20edc60dfa2f1c411
Aamir Shaikh <aamirshaikh3232@outlook.com>
2023-06-09 00:58:22 +0530

    Add 20_Day_Writing_clean_codes/exercises/level3/

---
+++ 20_Day_Writing_clean_codes/exercises/level3/level3.js
@@ -0,0 +1,5 @@
+// 1. Write a program that generates a random password with the following requirements: at least one uppercase letter, one lowercase letter, one digit, and one special character.
+
+// 2. Create a program that reads a text file and counts the number of words in it.
+
+// 3. Write a program that prompts the user to enter a sentence, then checks if the sentence is a palindrome.
